71a484d2eeaa08d7d31c3651272a27092ac7b026
cdanielss <cdanielss1973@outlook.com>
2020-07-28 16:09:37 -0300

    Segunda Aula

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+// npm install react-router-dom
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/Video'
+import CadastroCategoria from './pages/Cadastro/Categoria';
 
+// Criando pagina de erros
+const Pagina404 = () => (<div>Erro, Página404</div>)
 
+// Criação de Rotas
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
